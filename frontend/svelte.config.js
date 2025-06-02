8d6c14a0c983aa64116f8235083690ab16a15ce0
shubh <shubhvjain@gmail.com>
2025-06-02 14:23:46 +0530

    ..

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,7 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -11,7 +12,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths:{
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : "/"+process.env.BASE_PATH
 		}
 	},
 
