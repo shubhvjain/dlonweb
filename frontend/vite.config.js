6d8c197379ddf69c234e04fb3f2a07b8a73ee8ac
shubh <shubhvjain@gmail.com>
2025-06-11 09:22:01 +0200

    route fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path';
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, 'node_modules/dlonwebjs/dist/library'),  // source folder in your package
+          dest: '' // target folder inside `static/` after build
+        }
+      ]
+    })
+
+	]
 });
