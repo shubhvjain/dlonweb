8ec706be60b8c829814f2d8e6b5d36568286f1d4
shubh <shubhvjain@gmail.com>
2025-07-02 18:38:49 +0200

    fixing deployment

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,27 +1,23 @@
 import { mdsvex } from 'mdsvex';
-// import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-netlify';
-
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			edge:false,
-			split:false
-		}),
-		//paths: {
-		//	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		//}
-	},
+  extensions: ['.svelte', '.svx'],
+  
+  preprocess: [
+    vitePreprocess(),
+    mdsvex()
+  ],
 
-	preprocess: [mdsvex()],
-	extensions: ['.svelte', '.svx']
+  kit: {
+    adapter: adapter({
+      edge: false,
+      split: false
+    }),
+    // If you want to configure paths or other kit options, add here
+  }
 };
 
 export default config;
