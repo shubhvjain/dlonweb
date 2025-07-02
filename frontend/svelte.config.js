89577a45a860d365a09c345afe2479a5f66081e5
shubh <shubhvjain@gmail.com>
2025-07-02 15:37:30 +0200

    testing url fix

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -11,8 +11,8 @@ const config = {
 		adapter: adapter({
 			fallback: '404.html'
 		}),
-		paths:{
-			base: process.argv.includes('dev') ? '' : '/dlonweb1'
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	},
 
